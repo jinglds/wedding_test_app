31a831b1a61d56430e847ef42d024645e631f6a6
Jing <ladasa.tira@gmail.com>
2014-10-19 22:33:26 +0700

    try to heroku

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
-
+  before_action :correct_user,   only: :destroy
 
   def create
   	@event = current_user.events.build(event_params)
@@ -13,6 +13,8 @@ class EventsController < ApplicationController
   end
 
   def destroy
+    @event.destroy
+    redirect_to root_url
   end
 
   private
@@ -24,5 +26,10 @@ class EventsController < ApplicationController
   								:budget,
   								:bride_name,
   								:groom_name)
+  end
+
+    def correct_user
+        @event = current_user.events.find_by(id: params[:id])
+        redirect_to root_url if @event.nil?
   end
 end
